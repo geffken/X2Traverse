0aed2f1260bc053f902092656b648b23a29ca68d
Manuel Geffken <geffken@informatik.uni-freiburg.de>
2013-11-26 15:44:01 +0100

    Added usage.

--- src/de/unifr/acp/trafo/Main.java
+++ src/de/unifr/acp/trafo/Main.java
@@ -17,17 +17,29 @@ public class Main {
      * @throws ClassNotFoundException 
      */
     public static void main(String[] args) throws NotFoundException, ClassNotFoundException, IOException, CannotCompileException {
-        String className = args[0];
-        String outputDir = (args.length >= 2) ? args[1] : "bin";
-        ClassPool defaultPool = ClassPool.getDefault();
-        CtClass target = defaultPool.get(className);
-//        TransClass.doTransform(target, !target.getSuperclass().equals(objectClass));
-        TransClass.transformAndFlushHierarchy(className, outputDir);
+        try {
+            String className = args[0];
+            String outputDir = (args.length >= 2) ? args[1] : "bin";
+            ClassPool defaultPool = ClassPool.getDefault();
+            CtClass target = defaultPool.get(className);
+            // TransClass.doTransform(target,
+            // !target.getSuperclass().equals(objectClass));
+            TransClass.transformAndFlushHierarchy(className, outputDir);
 
-        // condition needed to avoid bug in writing unmodified class files causing invalid class files
-//        if (target.isModified()) { 
-//            target.writeFile(outputDir);
-//        }
+            // condition needed to avoid bug in writing unmodified class files
+            // causing invalid class files
+            // if (target.isModified()) {
+            // target.writeFile(outputDir);
+            // }
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            StringBuilder usage = new StringBuilder();
+            usage.append("Usage: x2traverse class [output directory]\n");
+            usage.append(" (to transform the class hierarchy rootet at a class\n");
+            usage.append("where options include:\n");
+            usage.append(" \n");
+            System.out.println(usage);
+        }
 
     }
 
