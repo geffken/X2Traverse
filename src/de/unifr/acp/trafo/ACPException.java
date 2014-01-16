8256003b56a8fac6dc331586bd0e762209d7aa67
Manuel Geffken <geffken@informatik.uni-freiburg.de>
2014-01-16 13:57:49 +0100

    Added exception class for ACP violations.

---
+++ src/de/unifr/acp/trafo/ACPException.java
@@ -0,0 +1,35 @@
+package de.unifr.acp.trafo;
+
+import de.unifr.acp.fst.Permission;
+import de.unifr.acp.templates.Global;
+
+public class ACPException extends RuntimeException {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -368923782328410522L;
+    
+    public ACPException(Object obj, String qualifiedFieldName,
+            String methodName, Permission effectivePerm, Permission requiredPerm) {
+        super(message(obj, qualifiedFieldName, methodName, effectivePerm, requiredPerm));
+        
+    }
+    
+    public static String message(Object obj, String qualifiedFieldName,
+            String methodName, Permission effectivePerm, Permission requiredPerm) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("ACCESS VIOLATION:\n");
+        sb.append("Instance: " + System.identityHashCode(obj)
+                + " of type " + obj.getClass().getSimpleName() + "\n");
+        sb.append("Field: " + qualifiedFieldName+"\n");
+        sb.append("Effective permission: " + effectivePerm + "\n");
+        sb.append("Required permission: " + requiredPerm + "\n");
+        sb.append("in method " + methodName + "\n");
+        sb.append(Global.newObjectsStack+"\n");
+        sb.append(Global.objPermStack+"\n");
+        sb.append("\n");
+        return sb.toString();
+    }    
+
+}
