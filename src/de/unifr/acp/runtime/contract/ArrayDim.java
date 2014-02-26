0618f9df227d9f161b8343d7a80f533193b89821
Manuel Geffken <geffken@informatik.uni-freiburg.de>
2014-02-26 15:14:30 +0100

    Updated javadoc for contracts.

--- src/de/unifr/acp/runtime/contract/ArrayDim.java
+++ src/de/unifr/acp/runtime/contract/ArrayDim.java
@@ -1,5 +1,6 @@
 package de.unifr.acp.runtime.contract;
 
+/** Special (invalid Java) identifier representing one array dimension. */
 public class ArrayDim extends Identifier {
     public ArrayDim() {
         super("[]");
--- src/de/unifr/acp/runtime/contract/AtLit.java
+++ src/de/unifr/acp/runtime/contract/AtLit.java
@@ -1,6 +1,6 @@
 package de.unifr.acp.runtime.contract;
 
-/** A special invalid identifier */
+/** A special (invalid Java) identifier representing the @ symbol. */
 public class AtLit extends Identifier {
 
     public AtLit() {
