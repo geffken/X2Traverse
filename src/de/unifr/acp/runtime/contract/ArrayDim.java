415ffa460231f28b83fdd840ca1ea3f0c485fd9f
Manuel Geffken <geffken@informatik.uni-freiburg.de>
2014-02-26 15:19:09 +0100

    Extended ArrayDim javadoc.

--- src/de/unifr/acp/runtime/contract/ArrayDim.java
+++ src/de/unifr/acp/runtime/contract/ArrayDim.java
@@ -1,6 +1,10 @@
 package de.unifr.acp.runtime.contract;
 
-/** Special (invalid Java) identifier representing one array dimension. */
+/**
+ * Special (invalid Java) identifier representing one array dimension. E.g. a
+ * two dimensional array field can be represented as
+ * Id("field").(ArrayDim()).(ArrayDim())
+ */
 public class ArrayDim extends Identifier {
     public ArrayDim() {
         super("[]");
